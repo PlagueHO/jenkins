49e389e6abb64d9bdaaebb2c06eb28e2dc3077c0
Daniel Scott-Raynsford <plagueho@gmail.com>
2017-11-10 16:14:31 +1300

    Added support for TLS 1.2 Jenkins Servers

--- Jenkins.psm1
+++ Jenkins.psm1
@@ -78,6 +78,30 @@ function New-Exception
     } # if
 } # function New-Exception
 
+<#
+.SYNOPSIS
+    Enables PowerShell to support TLS1.2 for communicating with
+    newer versions of Jenkins.
+.DESCRIPTION
+    This support cmdlet enables connecting to newer versions of
+    Jenkins over HTTPS. Newer versions of Jenkins have deprecated
+    support for SSL3/TLS, which are the default supported HTTPS
+    protocols.
+.OUTPUTS
+    None
+#>
+function Set-JenkinsTLSSupport
+{
+    [CmdLetBinding()]
+    param
+    (
+    )
+
+    if (-not ([Net.ServicePointManager]::SecurityProtocol).ToString().Contains([Net.SecurityProtocolType]::Tls12)) {
+        [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol + [Net.SecurityProtocolType]::Tls12
+    }
+} # function Set-JenkinsTLSSupport
+
 
 <#
 .SYNOPSIS
@@ -195,6 +219,8 @@ function Get-JenkinsCrumb()
     try {
         Write-Verbose -Message $($LocalizedData.GetCrumbMessage -f
             $FullUri)
+
+        Set-JenkinsTLSSupport
 
         $Result = Invoke-WebRequest `
             -Uri $FullUri `
@@ -390,6 +416,8 @@ function Invoke-JenkinsCommand()
                 Write-Verbose -Message $($LocalizedData.InvokingRestApiCommandMessage -f
                     $FullUri)
 
+                Set-JenkinsTLSSupport
+
                 $Result = Invoke-RestMethod `
                     -Uri $FullUri `
                     -Headers $Headers `
@@ -410,6 +438,8 @@ function Invoke-JenkinsCommand()
             try {
                 Write-Verbose -Message $($LocalizedData.InvokingRestApiCommandMessage -f
                     $FullUri)
+
+                Set-JenkinsTLSSupport
 
                 $Result = Invoke-RestMethod `
                     -Uri $FullUri `
@@ -433,6 +463,8 @@ function Invoke-JenkinsCommand()
 
             Write-Verbose -Message $($LocalizedData.InvokingCommandMessage -f
                 $FullUri)
+
+            Set-JenkinsTLSSupport
 
             $Result = Invoke-WebRequest `
                 -Uri $FullUri `
@@ -462,6 +494,8 @@ function Invoke-JenkinsCommand()
             try {
                 Write-Verbose -Message $($LocalizedData.InvokingCommandMessage -f
                     $FullUri)
+
+                Set-JenkinsTLSSupport
 
                 $Result = Invoke-WebRequest `
                     -Uri $FullUri `
@@ -2096,6 +2130,8 @@ function Initialize-JenkinsUpdateCache()
     # Download the Remote Update Center JSON
     Write-Verbose -Message $($LocalizedData.DownloadingRemoteUpdateListMessage -f
         $Uri)
+
+    Set-JenkinsTLSSupport
 
     $remotePluginJSON = Invoke-WebRequest `
         -Uri $Uri `
