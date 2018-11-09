464f746bc59bf9aad7bc017bc0d723dce0ad4c32
Aaron Jensen <ajensen@webmd.net>
2018-11-08 17:46:01 -0800

    Creating Disable-JenkinsJob and Enable-JenkinsJob for enabling and disabling jobs.

---
+++ src/lib/Enable-JenkinsJob.ps1
@@ -0,0 +1,69 @@
+
+function Enable-JenkinsJob
+{
+    [CmdLetBinding(SupportsShouldProcess = $true)]
+    [OutputType([System.String])]
+    param
+    (
+        [parameter(
+            Position = 1,
+            Mandatory = $true)]
+        [System.String]
+        $Uri,
+
+        [parameter(
+            Position = 2,
+            Mandatory = $false)]
+        [ValidateNotNullOrEmpty()]
+        [System.Management.Automation.PSCredential]
+        [System.Management.Automation.CredentialAttribute()]
+        $Credential,
+
+        [parameter(
+            Position = 3,
+            Mandatory = $false)]
+        [ValidateNotNullOrEmpty()]
+        [System.String]
+        $Crumb,
+
+        [parameter(
+            Position = 4,
+            Mandatory = $false)]
+        [ValidateNotNullOrEmpty()]
+        [System.String]
+        $Folder,
+
+        [parameter(
+            Position = 5,
+            Mandatory = $true)]
+        [ValidateNotNullOrEmpty()]
+        [System.String]
+        $Name
+    )
+
+    $null = $PSBoundParameters.Add('Type', 'Command')
+
+    $Command = Resolve-JenkinsCommandUri -Folder $Folder -JobName $Name -Command 'enable'
+
+    $optionalParams = @{}
+    if( $Credential )
+    {
+        $optionalParams['Credential'] = $Credential
+    }
+
+    if( $Crumb )
+    {
+        $optionalParams['Crumb'] = $Crumb
+    }
+
+    $displayName = $Name
+    if( $Folder )
+    {
+        $displayName = '{0}/{1}' -f $Folder,$Name
+    }
+
+    if ($PSCmdlet.ShouldProcess( $Uri, $($LocalizedData.EnableJobMessage -f $displayName)))
+    {
+        $null = Invoke-JenkinsCommand -Uri $Uri -Type 'Command' -Command $Command -Method post @optionalParams
+    }
+}
